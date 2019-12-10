a75e61c771a3bfd61e101377cf42164be132feb5
RyomaTanaka <tanakaryoma1992@gmail.com>
2019-12-10 20:52:54 +0900

    create_users_table

--- Gemfile
+++ Gemfile
@@ -26,7 +26,6 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-gem 'devise'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
@@ -35,6 +34,9 @@ gem 'devise'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'devise'
+gem 'devise-i18n'
+gem 'rails-i18n'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,20 @@ module StudyGroup
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.test_framework  nil 
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
     # Don't generate system test files.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     config.generators.system_tests = nil
   end
 end
