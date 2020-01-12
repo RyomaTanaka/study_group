6d291124cde5dc1aa5156d2d486340c5d523c277
RyomaTanaka <tanakaryoma1992@gmail.com>
2020-01-12 14:32:28 +0900

    change_image_uploader

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID_2'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY_2'],
-    region: 'ap-northeast-1'
-  }
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  elsif Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID_2"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY_2"],
+      region: 'ap-northeast-1'
+    }
 
-  config.fog_directory  = 'study-group-rails'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/study-group-rails'
+    config.fog_directory  = 'study-group-rails'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/study-group-rails'
 end
