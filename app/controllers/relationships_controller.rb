aef7ab1d0c96e6b0fd3abe8a2b25aaa784ba66c6
RyomaTanaka <tanakaryoma1992@gmail.com>
2020-01-16 11:18:52 +0900

    change_read_me_url

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,16 @@ class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
+    @user = User.find(params[:user_id])
     respond_to do |format|
       format.js
     end
   end
-
+  
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
+    @user = User.find(params[:user_id])
     respond_to do |format|
       format.js
     end
