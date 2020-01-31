83be09988d9249990dd51240e537369397586c88
RyomaTanaka <tanakaryoma1992@gmail.com>
2020-01-31 21:34:58 +0900

    delete_binding.pry2

--- spec/system/follows_spec.rb
+++ spec/system/follows_spec.rb
@@ -9,9 +9,23 @@ RSpec.describe "Follows", type: :system do
       sign_in user
       other_user
       visit root_path
-      expect { click_button "フォロー" }.to change{ user.active_relationships.count }.by(1)
+      # expect { 
+      #   click_button "フォロー" 
+      # }.to change{ user.active_relationships.count }.by(1)
+      click_button "フォロー"
+      expect(page).to have_content "解除"
     end
   end
 
-  
+  describe "show", js: true do
+    scenario "フォローしたユーザー一覧を見ることができる" do
+      sign_in user
+      other_user
+      visit root_path
+      click_button "フォロー"
+      click_link "マイページ"
+      find(".follow").click 
+      expect(page).to have_content "#{other_user.nickname}"
+    end
+  end
 end
