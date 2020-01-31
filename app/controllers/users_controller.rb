392dfda5c2a00adffaac536a2370ecbe02c0be16
RyomaTanaka <tanakaryoma1992@gmail.com>
2020-01-31 19:36:12 +0900

    delete_binding.pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,6 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      binding.pry
       redirect_to user_path(@user), notice: "プロフィールが編集されました。"
     else
       render action: :edit
